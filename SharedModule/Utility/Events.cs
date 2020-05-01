61535bdb47e3d72741ec50d953c9bab2bdab41ab
Hakan FISTIK <hakam.fostok@biteg.net>
2020-05-01 21:28:37 +0300

    Add Events File

---
+++ SharedModule/Utility/Events.cs
@@ -0,0 +1,10 @@
+using Prism.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharedModule
+{
+    // define all the events of the application here
+    // public class SomeEvent : PubSubEvent { }
+}
